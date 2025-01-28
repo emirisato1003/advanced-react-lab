dfd1f6266dda351aefa566480374f33d65dd1469
emirisato1003 <emirisato.1003@gmail.com>
2025-01-28 11:33:34 -0800

    fix code more concisely

--- advanced-react-lab/src/App.jsx
+++ advanced-react-lab/src/App.jsx
@@ -4,9 +4,12 @@ import Button from "./Button";
 function App() {
   return (
     <main>
+      <Button>Log in with Google</Button>
+      <Button variant="success" size="sm" className="green">Log in with Google</Button>
+      <Button variant="warning" size="lg" className="green">Log in with Google</Button>
       <Button variant="danger" size="lg" className="green">Log in with Google</Button>
     </main>
   );
 }
 
-export default App
+export default App;
--- advanced-react-lab/src/Button.jsx
+++ advanced-react-lab/src/Button.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import classNames from "classnames";
 export default function Button({ children, size, className, variant, ...rest }) {
 
-    let sizeClass = size ? `button-${size}` : ""
+    let sizeClass = size && `button-${size}`
+    let variantClass = variant && `button-${variant}`
 
-    const allClasses = classNames(sizeClass, className, variant);
+    const allClasses = classNames(sizeClass, className, variantClass);
 
     return (
         <button className={allClasses} {...rest}>
