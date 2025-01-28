67ee5cc67dadbe138637679150cb46d880d89a3b
emirisato1003 <e.sato@students.clark.edu>
2025-01-28 15:34:39 -0800

    pull the code and update from different computer.

--- advanced-react-lab/src/App.jsx
+++ advanced-react-lab/src/App.jsx
@@ -4,10 +4,19 @@ import Button from "./Button";
 function App() {
   return (
     <main>
-      <Button>Log in with Google</Button>
-      <Button variant="success" size="sm" className="green">Log in with Google</Button>
-      <Button variant="warning" size="lg" className="green">Log in with Google</Button>
-      <Button variant="danger" size="lg" className="green">Log in with Google</Button>
+
+      <h1>Buttons</h1>
+      <div className="button-container">
+        <Button>Log in with Google</Button>
+        <Button variant="success" size="sm" className="green">Log in with Google</Button>
+        <Button variant="warning" size="lg" className="green">Log in with Google</Button>
+        <Button variant="danger" size="lg" className="green">Log in with Google</Button>
+      </div>
+
+      <h1>Avatars</h1>
+      <div className="avatar-container">
+
+      </div>
     </main>
   );
 }
--- advanced-react-lab/src/Button.jsx
+++ advanced-react-lab/src/Button.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import classNames from "classnames";
 export default function Button({ children, size, className, variant, ...rest }) {
 
-    let sizeClass = size && `button-${size}`
-    let variantClass = variant && `button-${variant}`
+    let sizeClass = size && `button-${size}`;
+    let variantClass = variant && `button-${variant}`;
 
     const allClasses = classNames(sizeClass, className, variantClass);
 
     return (
-        <button className={allClasses} {...rest}>
-            {children}
-        </button>
+        <div className="single-button">
+            <h2>Button: {size && variant ? `${size} + ${variant}` : 'original'}</h2>
+            <button className={allClasses} {...rest}>
+                {children}
+            </button>
+        </div>
     );
 }
