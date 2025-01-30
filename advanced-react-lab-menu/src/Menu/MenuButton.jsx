09dd64c2c6bb7dea58c106ffe4e5dfe31c1e3754
emirisato1003 <emirisato.1003@gmail.com>
2025-01-29 16:22:31 -0800

    convert Menu component to use props.children.

--- advanced-react-lab-menu/src/Menu/MenuButton.jsx
+++ advanced-react-lab-menu/src/Menu/MenuButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../Button/Button";
 
 export default function MenuButton({ buttonText, onClick }) {
+
     return (
         <Button onClick={onClick}>{buttonText}</Button>
     );
