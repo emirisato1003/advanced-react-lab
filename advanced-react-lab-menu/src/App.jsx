fe8baea0949b23c0c8c00fdbb0341f2847a0b1be
emirisato1003 <e.sato@students.clark.edu>
2025-01-29 22:17:01 -0800

    switch react children component. add MenuItem component.

--- advanced-react-lab-menu/src/App.jsx
+++ advanced-react-lab-menu/src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Menu from "./Menu/Menu";
 import MenuButton from "./Menu/MenuButton";
 import MenuDropdown from "./Menu/MenuDropdown";
+import MenuItem from './Menu/MenuItem';
 
 function App() {
+  const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
   return (
     <Menu>
       <MenuButton>Sports</MenuButton>
-      <MenuDropdown>{["Tennis", "Pickleball", "Racquetball", "Squash"]}</MenuDropdown>
+      <MenuDropdown>
+        {sports.map(sport =>
+          <MenuItem key={sport}>{sport}</MenuItem>
+        )}
+      </MenuDropdown>
     </Menu>
   );
 }
