8cd3332c5d2a17e2f33caea73a3054a686628ee2
emirisato1003 <emirisato.1003@gmail.com>
2025-01-30 17:51:18 -0800

    Set up React.Context

--- advanced-react-lab-theme/src/App.jsx
+++ advanced-react-lab-theme/src/App.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { createContext } from "react";
 import Header from "./Header";
 import Button from "./Button";
 
+const ThemeContext = createContext()
+
 export default function App() {
   return (
-    <div className="container dark-theme">
-      <Header />
-      <Button />
-    </div>
+    <ThemeContext.Provider value="light">
+      <div className="container dark-theme">
+        <Header />
+        <Button />
+      </div>
+    </ThemeContext.Provider>
   );
 }
+
+export { ThemeContext };
