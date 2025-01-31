9076dc7ce48c3e1a029482f98427ccd314ef9736
emirisato1003 <e.sato@students.clark.edu>
2025-01-31 00:04:07 -0800

    update css className

--- advanced-react-lab-theme/src/Header.jsx
+++ advanced-react-lab-theme/src/Header.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "./App";
 
 export default function Header() {
+    const value = useContext(ThemeContext);
+
     return (
-        <header className="dark-theme">
-            <h1>Dark Theme</h1>
+        <header className={`${value}-theme`}>
+            <h1>{`${value} Theme`}</h1>
         </header>
     );
 }
