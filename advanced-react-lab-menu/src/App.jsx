416493dc42e82632d592561831087b044dfb2a4b
emirisato1003 <emirisato.1003@gmail.com>
2025-02-01 14:48:53 -0800

    switch dot syntax of compound component

--- advanced-react-lab-menu/src/App.jsx
+++ advanced-react-lab-menu/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import Menu from "./Menu/Menu";
-import MenuButton from "./Menu/MenuButton";
-import MenuDropdown from "./Menu/MenuDropdown";
-import MenuItem from './Menu/MenuItem';
+import Menu from "./Menu/index";
+// import MenuButton from "./Menu/MenuButton";
+// import MenuDropdown from "./Menu/MenuDropdown";
+// import MenuItem from './Menu/MenuItem';
 
 function App() {
   const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
   return (
     <Menu>
-      <MenuButton>Sports</MenuButton>
-      <MenuDropdown>
+      <Menu.Button>Sports</Menu.Button>
+      <Menu.Dropdown>
         {sports.map(sport =>
-          <MenuItem key={sport}>{sport}</MenuItem>
+          <Menu.Item key={sport}>{sport}</Menu.Item>
         )}
-      </MenuDropdown>
+      </Menu.Dropdown>
     </Menu>
   );
 }
