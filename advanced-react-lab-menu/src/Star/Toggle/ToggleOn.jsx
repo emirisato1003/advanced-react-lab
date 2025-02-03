acbeb18b1ae293f2c398cce97a05ecdd62c2b174
emirisato1003 <emirisato.1003@gmail.com>
2025-02-02 20:13:25 -0800

    create ToggleOn and Off component.

---
+++ advanced-react-lab-menu/src/Star/Toggle/ToggleOn.jsx
@@ -0,0 +1,9 @@
+
+import { useContext } from "react";
+import { ToggleContext } from "./Toggle";
+
+export default function ToggleOn({ children }) {
+    const { on } = useContext(ToggleContext);
+
+    return on ? { children } : null;
+}
