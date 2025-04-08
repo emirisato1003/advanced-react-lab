8607dc7e1a59dae29e8b1f3a16a51fd6c390fc2e
Emiri Sato <emirisato.1003@gmail.com>
2025-04-07 18:10:38 -0700

    fixed styles

--- advanced-react-lab-Vanlife/src/App.jsx
+++ advanced-react-lab-Vanlife/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import Home from './assets/components/Home';
-import About from './assets/components/About';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 
--- advanced-react-lab-Vanlife/src/pages/Home.jsx
+++ advanced-react-lab-Vanlife/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
         <div className="hero">
             <div className="container">
                 <h1>You got the travel plans, we got the travel vans.</h1>
                 <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-                <button>Find your van</button>
+                <Link to="vans">Find your van</Link>
             </div>
         </div>
     );
