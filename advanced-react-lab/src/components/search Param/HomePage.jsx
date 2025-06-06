d29968fd19e947b9d67a785ca3972ccedd099dce
Emiri Sato <emirisato.1003@gmail.com>
2025-06-06 13:02:31 -0700

    add handle function for button filter.

--- advanced-react-lab/src/components/search Param/HomePage.jsx
+++ advanced-react-lab/src/components/search Param/HomePage.jsx
@@ -7,12 +7,12 @@ const swCharacters = [
     { name: "Yoda", type: "Jedi" }
 ];
 export default function HomePage() {
-    const [searchParams, setSearchParams] = useSearchParams()
-    const typeFilter = searchParams.get("type")
+    const [searchParams, setSearchParams] = useSearchParams();
+    const typeFilter = searchParams.get("type");
 
-    const filteredChar = typeFilter 
-    ? swCharacters.filter(({type}) => type.toLowerCase() === typeFilter) 
-    : swCharacters
+    const filteredChar = typeFilter
+        ? swCharacters.filter(({ type }) => type.toLowerCase() === typeFilter)
+        : swCharacters;
 
     const charEls = filteredChar.map(({ name, type }) => (
         <div key={name}>
@@ -20,12 +20,51 @@ export default function HomePage() {
             <p>Type: {type}</p>
         </div>
     ));
+
+    // create helper function called 'genNewSearchParamString' take a key and a value as a parameter
+    const genNewSearchParamString = (key, value) => {
+        // set new URLSearchParams constructor to brings up old value of searchParams and initialize new URL search param.
+        const sp = new URLSearchParams(searchParams);
+        //check if the value is null
+        if (value === null) {
+            // if it is, delete key
+            sp.delete(key);
+        } else {
+            // if it's not null, set the search param of key equal to whatever the value is that we passed in
+            sp.set(key, value);
+        }
+        // console.log(typeof sp.toString());
+        // return the string value using 'toString' method.
+        return `?${sp.toString()}`;
+    };
+
+    // create a handle function called 'handleFilterChange()', take 'key' and 'value' as its parameters
+    const handleFilterChange = (key, value) => {
+        //set a setter function of useSearchParams() and take a previous prams
+        setSearchParams(prevPram => {
+            //check if value is null
+            if (value === null) {
+                // take a previous prams  and delete key !!! useSearchParams() can modify previous prams
+                prevPram.delete(key);
+            } else {
+                // else : set key and value
+                prevPram.set(key, value);
+            }
+            // return prevParams since we want to return URL search params obj
+            return prevPram;
+        });
+    };
     return (
         <main>
             <h2>Home</h2>
-            <button onClick={() => setSearchParams({type: "jedi"})}>Jedi</button>
-            <button onClick={() => setSearchParams({type: "sith"})}>Sith</button>
-            <button onClick={() => setSearchParams({})}>Clear</button>
+            <div>
+                <Link to={genNewSearchParamString("type", "jedi")}>Jedi</Link>
+                <Link to={genNewSearchParamString("type", "sith")}>Sith</Link>
+                <Link to={genNewSearchParamString("type", null)}>Clear</Link>
+            </div>
+            <button onClick={() => handleFilterChange("type", "jedi")}>Jedi</button>
+            <button onClick={() => handleFilterChange("type", "sith")}>Sith</button>
+            <button onClick={() => handleFilterChange("type", null)}>Clear</button>
             {charEls}
         </main>
     );
